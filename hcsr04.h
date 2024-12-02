890073fae99c2097286d039cb569ed2d153da749
Nikolay <stepanenko_robot@mail.ru>
2024-12-02 23:23:52 +0300

    Update hcsr04.h

--- hcsr04.h
+++ hcsr04.h
@@ -1,24 +1,18 @@
 #ifndef _HC_SR04_H
 #define _HC_SR04_H
 #include "stm32f407xx.h"
-
-/*
- * При создании обработчика проверяем, является ли то, что мы создаем - EXTI9_5_IRQHandler. Если является, то делаем #define _EXTI_9_5_IN_STOCK.
- * Ну и при создании каждого последующего обработчика делаем эту проверку.
- * Массив индексов - нахуй. Сделаем просто функцию, которая будет искать номер модуля в зависимости от пина прерывания.
- */
-
-#include <stdint.h>
 typedef struct
 {
-    uint16_t time_before_echo;
-    uint16_t time_after_echo;
     uint16_t result_time;
+    uint16_t start_time;
+    uint16_t end_time;
+    uint8_t waiting_rising : 1;
     uint8_t is_waiting_echo : 1;
     uint8_t is_new_time : 1;
-    uint8_t ECHO_pin : 6;
+    uint8_t echo_pin : 5;
+    GPIO_TypeDef* trigger_port;
+    uint8_t trigger_pin;
 } hc_sr04_t;
-
 
 
 #define _HC_SR04_PORTA                  0
@@ -32,51 +26,88 @@ typedef struct
 #define _HC_SR04_PORTI                  8
 
 
+#define __HC_SR04_DOUBLE_EXTI           0
+#define __HC_SR04_EXTI_0                1
+#define __HC_SR04_EXTI_1                2
+#define __HC_SR04_EXTI_2                3
+#define __HC_SR04_EXTI_3                4
+#define __HC_SR04_EXTI_4                5
+#define __HC_SR04_EXTI_9_5              6
+#define __HC_SR04_EXTI_15_10            7
+
+
+
+#define _HC_SR04_APB1                   1
+#define _HC_SR04_APB2                   2
+
+
+
 
 //TIM defines
 #define _HC_SR04_TIM                    TIM2
 #define _HC_SR04_TIM_IRQn               TIM2_IRQn
 #define _HC_SR04_TIM_IRQHandler         TIM2_IRQHandler
-#define _HC_SR04_TIM_BUS_FREQ_HZ        10000
+#define _HC_SR04_TIM_BUS                _HC_SR04_APB1
+#define _HC_SR04_TIM_BUS_DIV            4UL
+#define _HC_SR04_TIM_BUS_FREQ_HZ        10000UL
 
+
+
+//WARNING: YOU CANT MAKE THE SAME IRQHandlers FOR TWO OR MORE SENSORS, thanks. If there 2 or more same IRQHandlers, u need  _HC_SR04_EXTI_n (where n is number of sensor)
+//                                                          define as __HC_SR04_DOUBLE_EXTI.
 
 
 //Numbers of sensors define
 #define _HC_SR04_SENSORS_NUM            1
 
 //ECHO and TRIG port and pins defines for FIRST SENSOR
-#define _HC_SR04_ECHO_PIN_1             5
-#define _HC_SR04_TRIG_PIN_1             4
+#if _HC_SR04_SENSORS_NUM >= 1
+#define _HC_SR04_ECHO_PIN_1             11UL
+#define _HC_SR04_TRIG_PIN_1             4UL
 #define _HC_SR04_ECHO_PORT_1            GPIOA
 #define _HC_SR04_TRIG_PORT_1            GPIOB
-#define _HC_SR04_EXTI_IRQn_1            EXTI0_IRQn
-#define _HC_SR04_EXTI_IRQHandler_1      EXTI0_IRQHandler
-
+#define _HC_SR04_EXTI_1                 __HC_SR04_EXTI_15_10
+#define _HC_SR04_EXTI_IRQn_1            EXTI15_10_IRQn
+#define _HC_SR04_EXTI_IRQHandler_1      EXTI15_10_IRQHandler
+#endif
 
 //ECHO and TRIG port and pins defines for SECOND SENSOR
 #if _HC_SR04_SENSORS_NUM >= 2
-#define _HC_SR04_ECHO_PIN_2             2
-#define _HC_SR04_TRIG_PIN_2             3
+#define _HC_SR04_ECHO_PIN_2             2UL
+#define _HC_SR04_TRIG_PIN_2             3UL
 #define _HC_SR04_ECHO_PORT_2            GPIOF
-#define _HC_SR04_TIRG_PORT_2            GPIOD
-#define _HC_SR04_EXTI_IRQn_2
-#define _HC_SR04_EXTI_IRQHandler_2
+#define _HC_SR04_TRIG_PORT_2            GPIOD
+#define _HC_SR04_EXTI_2                 __HC_SR04_EXTI_2
+#define _HC_SR04_EXTI_IRQn_2            EXTI2_IRQn
+#define _HC_SR04_EXTI_IRQHandler_2      EXTI2_IRQHandler
 #endif
+
 
 
 //ECHO and TRIG port and pins defines for THIRD SENSOR
 #if _HC_SR04_SENSORS_NUM >= 3
-#define _HC_SR04_ECHO_PIN_3             2
+#define _HC_SR04_ECHO_PIN_3             3
 #define _HC_SR04_TRIG_PIN_3             3
 #define _HC_SR04_ECHO_PORT_3            GPIOF
-#define _HC_SR04_TIRG_PORT_3            GPIOD
-#define _HC_SR04_EXTI_IRQn_3
-#define _HC_SR04_EXTI_IRQHandler_3
+#define _HC_SR04_TIRG_PORT_3            GPIOA
+#define _HC_SR04_EXTI_3                 __HC_SR04_EXTI_3
+#define _HC_SR04_EXTI_IRQn_3            EXTI3_IRQn
+#define _HC_SR04_EXTI_IRQHandler_3      EXTI3_IRQHandler
 #endif
 
 
-/*Так как все добавляется по очереди, то в обработчике прерываний для 1 сенсора будет - sensors[0], для 2го - sensors[1] и так далее.
- * Нужно только разобраться с
- */
+
+
+
+//NOTE: THIS PART YOU SHOULDNT TOUCH
+void hcsr_04_init(void);
+void _HC_SR04_TIM_IRQHandler(void);
+void _HC_SR04_EXTI_IRQHandler_1(void);
+#if _HC_SR04_SENSORS_NUM >= 2 && _HC_SR04_EXTI_1
+void _HC_SR04_EXTI_IRQHandler_2(void);
+#endif
+#if _HC_SR04_SENSORS_NUM >= 3 && _HC_SR04_EXTI_3
+void _HC_SR04_EXTI_IRQHandler_3(void);
+#endif
 #endif
 
